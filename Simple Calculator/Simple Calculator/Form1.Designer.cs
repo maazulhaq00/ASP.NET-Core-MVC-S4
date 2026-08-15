67e66646176da3f3c09e3d2dd2af27401fa3ff36
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-08-15 09:24:45 +0500

    WFA

---
+++ Simple Calculator/Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1Label = new System.Windows.Forms.Label();
+            this.num2Label = new System.Windows.Forms.Label();
+            this.num1TextBox = new System.Windows.Forms.TextBox();
+            this.num2TextBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1Label
+            // 
+            this.num1Label.AutoSize = true;
+            this.num1Label.Location = new System.Drawing.Point(55, 41);
+            this.num1Label.Name = "num1Label";
+            this.num1Label.Size = new System.Drawing.Size(66, 13);
+            this.num1Label.TabIndex = 0;
+            this.num1Label.Text = "First Number";
+            // 
+            // num2Label
+            // 
+            this.num2Label.AutoSize = true;
+            this.num2Label.Location = new System.Drawing.Point(55, 88);
+            this.num2Label.Name = "num2Label";
+            this.num2Label.Size = new System.Drawing.Size(84, 13);
+            this.num2Label.TabIndex = 1;
+            this.num2Label.Text = "Second Number";
+            // 
+            // num1TextBox
+            // 
+            this.num1TextBox.Location = new System.Drawing.Point(171, 38);
+            this.num1TextBox.Name = "num1TextBox";
+            this.num1TextBox.Size = new System.Drawing.Size(173, 20);
+            this.num1TextBox.TabIndex = 2;
+            // 
+            // num2TextBox
+            // 
+            this.num2TextBox.Location = new System.Drawing.Point(171, 85);
+            this.num2TextBox.Name = "num2TextBox";
+            this.num2TextBox.Size = new System.Drawing.Size(173, 20);
+            this.num2TextBox.TabIndex = 3;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(269, 136);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(55, 203);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.num2TextBox);
+            this.Controls.Add(this.num1TextBox);
+            this.Controls.Add(this.num2Label);
+            this.Controls.Add(this.num1Label);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1Label;
+        private System.Windows.Forms.Label num2Label;
+        private System.Windows.Forms.TextBox num1TextBox;
+        private System.Windows.Forms.TextBox num2TextBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Simple Calculator/Simple Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if ( string.IsNullOrEmpty(num1TextBox.Text) || string.IsNullOrEmpty(num2TextBox.Text) ) 
+            {
+                MessageBox.Show("All feilds are required");
+            }
+            else
+            {
+                int num1 = int.Parse(num1TextBox.Text);
+                int num2 = int.Parse(num2TextBox.Text);
+
+                int result = num1 + num2;
+
+                resultLabel.Text = $"The summition is {result}";
+
+                clearForm();
+
+            }
+        }
+
+        private void clearForm()
+        {
+            num1TextBox.Text = "";
+            num2TextBox.Text = "";
+        }
+
+    }
+}
