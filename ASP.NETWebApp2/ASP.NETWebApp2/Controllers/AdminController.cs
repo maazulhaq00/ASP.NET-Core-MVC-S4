0efd07e31ea8e0b12574fc18b1c30eefaaee8d23
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-08-15 09:30:11 +0500

    controllers, action methods, razor

---
+++ ASP.NETWebApp2/ASP.NETWebApp2/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETWebApp2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NETWebApp2/ASP.NETWebApp2/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETWebApp2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
+
