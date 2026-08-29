111e6d65e9115bc90ac2b88f4f158dcdb60ce90d
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-08-29 17:31:07 +0500

    strongly typed views

---
+++ 6. Strongly Typed ViewsW/Strongly Typed ViewsW/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Strongly_Typed_ViewsW.Models;
+
+namespace Strongly_Typed_ViewsW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayEmployee()
+        {
+            var emp = new Employee { EmpId = 1, EmpName = "Khalid", Designation = "Graphics Designer", Salary = 75000 };
+
+            return View(emp);
+        }
+        public IActionResult DisplayEmployeeList()
+        {
+
+            var empList = new List<Employee>
+            {
+                new Employee { EmpId = 1, EmpName = "Khalid", Designation = "Graphics Designer", Salary = 75000 },
+                new Employee { EmpId = 1, EmpName = "Ali", Designation = "PHP Developer", Salary = 85000 },
+                new Employee { EmpId = 1, EmpName = "Sarah", Designation = "Website Developer", Salary = 65000 },
+                new Employee { EmpId = 1, EmpName = "Zain", Designation = "Software Engineer", Salary = 95000 },
+                new Employee { EmpId = 1, EmpName = "Fawad", Designation = "HR Officer", Salary = 55000 }
+
+            };
+
+            return View("DisplayEmployees", empList);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 6. Strongly Typed ViewsW/Strongly Typed ViewsW/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Strongly_Typed_ViewsW.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
