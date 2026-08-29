486b97ca4c7d58618e75f9a31df69eb526c57262
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-08-29 17:27:48 +0500

    Temp Data, Keep, Redirection

---
+++ 3. TempDataW/TempDataW/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TempDataW.Models;
+
+namespace TempDataW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["data1"] = "View Data";
+            ViewBag.data2 = "View Bag";
+            TempData["data3"] = "Temp Data";
+
+            TempData.Keep();
+
+            //return RedirectToAction("About");
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 3. TempDataW/TempDataW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TempDataW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
